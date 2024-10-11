3a4d58655607466466c36f489ec2bbf6af6657a6
salmanahadyy <ahady1105@gmail.com>
2024-10-11 09:28:08 +0700

    day7

---
+++ exercise day7/exercise2.ts
@@ -0,0 +1,16 @@
+class Player  {
+    name = ""
+health = 0
+power = 0
+    
+    
+    constructor(nama:string, Health:number = 100, Power: number = 10) {
+        this.name = nama
+        this.health = Health
+        this.power = Power
+    }
+}
+
+class ShootingGame {
+    
+}
---
+++ exercise day7/maxChar.ts
@@ -0,0 +1,26 @@
+/*
+
+input:
+str = "Purwadhika Bandung"
+n = 10
+
+output : "Purwadhika..."
+
+input:
+str = "Purwadhika Bandung"
+n = 15
+
+output : "Purwadhika Band"
+
+
+input:
+str = "Purwadhika Bandung"
+n = 20
+
+output : "purwadhika Bandung"
+
+*/
+
+const str = "Purwadhika Bandung"
+
+console.log(str.substring(0,15) + "...")
